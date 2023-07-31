b40f70ff15979d497f7928a77b71fcf85709b36c
edson <edsondsouzadev@gmail.com>
2023-07-31 10:27:45 +0530

    added hello_world and hello_cargo folders

---
+++ projects/hello_cargo/target/debug/deps/hello_cargo-fd8a25d5961bc46b.d
@@ -0,0 +1,5 @@
+C:\workspace\rust-crash-course\projects\hello_cargo\target\debug\deps\hello_cargo-fd8a25d5961bc46b.rmeta: src\main.rs
+
+C:\workspace\rust-crash-course\projects\hello_cargo\target\debug\deps\hello_cargo-fd8a25d5961bc46b.d: src\main.rs
+
+src\main.rs:
